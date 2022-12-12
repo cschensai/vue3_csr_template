ac0564d28aa29ee8623635575cfbfe96c55aeb0f
chensai <cs1324332517@163.com>
2022-12-12 10:45:38 +0800

    vue meta

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,31 @@
 <script setup>
+import { useMeta } from 'vue-meta';
 import { ElConfigProvider } from 'element-plus';
 import elementPlusId from 'element-plus/es/locale/lang/id';
 import elementPlusEn from 'element-plus/es/locale/lang/en';
 import { onMounted } from "vue";
 import { useStore } from "vuex";
 const store = useStore();
+
+// TODO: meta 示例信息
+useMeta(
+  {
+    title: 'Desty Page',
+    // description: 'The Description',
+    og: {
+      title: 'Og Title',
+      description: 'Bla bla',
+      image: [
+        'https://picsum.photos/600/400/?image=80',
+        'https://picsum.photos/600/400/?image=82'
+      ]
+    },
+    twitter: {
+      title: 'Twitter Title',
+    },
+  },
+)
+
 onMounted(() => {
   if (document.body.clientWidth < 768) {
     store.commit("setIsMobileMode", true);
@@ -20,6 +41,8 @@ onMounted(() => {
 </script>
 
 <template>
+  <!-- 必须声明 -->
+  <metainfo />
   <el-config-provider :locale="$store.state.language === 'id' ? elementPlusId : elementPlusEn">
     <div>
         <router-view></router-view>
