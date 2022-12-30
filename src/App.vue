1d477ff94773a7d18f12ebead4a4e68a19c98e57
chensai <cs1324332517@163.com>
2022-12-30 16:16:30 +0800

    feat: metainfo示例

--- src/App.vue
+++ src/App.vue
@@ -48,7 +48,38 @@ onMounted(() => {
 
 <template>
   <!-- 必须声明 -->
-  <metainfo />
+  <metainfo>
+    <!-- <component :is="'script'" type="application/ld+json">
+      {
+      "@context": "https://schema.org/",
+      "@type": "WebPage",
+      "name": "desty omni | Omnichannel untuk Atur Semua Marketplace Kamu",
+      "url": "https://omni.desty.app/commerce/omniregisterlp",
+      "description": "Mudah atur semua marketplace lewat satu dashboard dengan Desty Omni. Atur stok produk, balas chat,
+      proses pesanan makin cepat di satu tempat.",
+      "image": "",
+      "identifier": "desty omni | Omnichannel untuk Atur Semua Marketplace Kamu",
+      "alternateName": "Desty Omni",
+      "significantLink": "https://omni.desty.app/commerce/omniregisterlp",
+      "dateCreated": "",
+      "isPartOf": "https://desty.omni",
+      "thumbnailUrl": "" }
+    </component>
+    <component :is="'link'" rel="canonical" href="https://omni.desty.app" />
+    <component :is="'link'" rel="alternate" href="https://omni.desty.app/commerce/omniregisterlp" hreflang="id-id" />
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://omni.desty.app/commerce/omniregisterlp">
+    <meta property="twitter:title" content="desty omni | Omnichannel untuk Atur Semua Marketplace Kamu">
+    <meta property="twitter:description"
+      content="Mudah atur semua marketplace lewat satu dashboard dengan Desty Omni. Atur stok produk, balas chat, proses pesanan makin cepat di satu tempat.">
+    <meta property="twitter:image" content="https://www.desty.app/favicon.ico?v=2">
+    <meta name="twitter:domain" content="Desty Omni">
+    <meta name="Page title" content="desty omni | Omnichannel untuk Atur Semua Marketplace Kamu">
+
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="viewport"
+      content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"> -->
+  </metainfo>
   <el-config-provider :locale="$store.state.language === 'id' ? elementPlusId : elementPlusEn">
     <div>
       <router-view></router-view>
